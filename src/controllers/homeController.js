64671448e2389480bf60795aab9d258842cf428f
Daniel Uzunski <dani.pleven@icloud.com>
2025-01-19 23:32:23 +0200

    Added createMovie page

--- src/controllers/homeController.js
+++ src/controllers/homeController.js
@@ -4,7 +4,7 @@ import movies from '../moviesData.js'
 const router = express.Router();
 
 router.get("/", (req, res) => {
-  res.render("home", { movies });
+  res.render("home", { movies }); //Temporary 
 });
 
 router.get("/about", (req, res) => {
--- src/routes.js
+++ src/routes.js
@@ -1,9 +1,11 @@
 import express from 'express';
-import homeController from './controllers/homeController.js'
+import homeController from './controllers/homeController.js';
+import createMovieController from './controllers/createMovieController.js'
 
 const routes = express.Router();
 
 routes.use(homeController);
+routes.use(createMovieController)
 
 routes.get('*', (req, res) => {
     res.render('404');
