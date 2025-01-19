809731df44a378a96fc871dce850b93545f4af71
Daniel Uzunski <dani.pleven@icloud.com>
2025-01-19 22:22:31 +0200

    Added about page view

--- src/index.js
+++ src/index.js
@@ -1,18 +1,28 @@
-import express from 'express';
-import handlebars from 'express-handlebars';
+import express from "express";
+import handlebars from "express-handlebars";
 
 const app = express();
 
-app.engine('hbs', handlebars.engine({
-  extname: 'hbs'
-}));
-app.set('view engine', 'hbs');
-app.set('views', './src/views');
+app.engine(
+  "hbs",
+  handlebars.engine({
+    extname: "hbs",
+  })
+);
 
-app.use('/static', express.static('src/public'));
+app.set("view engine", "hbs");
+app.set("views", "./src/views");
 
-app.get('/', (req, res) => {
-  res.render('home', {layout: false})
+app.use("/static", express.static("src/public"));
+
+app.get("/", (req, res) => {
+  res.render("home", { layout: false });
 });
 
-app.listen(5000, () => console.log('Server listening on http://localhost:5000...'))
+app.get("/about", (req, res) => {
+  res.render("about", { layout: false });
+});
+
+app.listen(5000, () =>
+  console.log("Server listening on http://localhost:5000...")
+);
