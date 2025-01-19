d76722918af2bc28c32877300a505a0323e7cc78
Daniel Uzunski <dani.pleven@icloud.com>
2025-01-19 22:29:40 +0200

    Added 404 page

--- src/index.js
+++ src/index.js
@@ -23,6 +23,10 @@ app.get("/about", (req, res) => {
   res.render('about');
 });
 
+app.get('/*', (req, res) => {
+  res.render('404');
+})
+
 app.listen(5000, () =>
   console.log("Server listening on http://localhost:5000...")
 );
