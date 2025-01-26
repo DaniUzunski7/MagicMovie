481ad4f30cbf4117bec86ea33bf977f3d3a2e7ce
Daniel Uzunski <dani.pleven@icloud.com>
2025-01-26 15:51:44 +0200

    Added castCreate page
    added castController
    Navigation update

--- src/routes.js
+++ src/routes.js
@@ -1,11 +1,13 @@
 import express from 'express';
 import homeController from './controllers/homeController.js';
 import movieController from './controllers/movieController.js'
+import castController from './controllers/castController.js'
 
 const routes = express.Router();
 
 routes.use(homeController);
-routes.use('/movies', movieController)
+routes.use('/movies', movieController);
+routes.use('/cast', castController);
 
 routes.get('*', (req, res) => {
     res.render('404');
