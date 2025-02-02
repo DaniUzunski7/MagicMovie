cc420d6decf9e842a6d28f94682036dccd55732f
Daniel Uzunski <dani.pleven@icloud.com>
2025-02-02 14:59:35 +0200

    Send cookie token to client

--- src/controllers/authController.js
+++ src/controllers/authController.js
@@ -3,37 +3,36 @@ import { authServices } from "../services/authService.js";
 
 const authController = express.Router();
 
-authController.get('/register', (req, res) => {
+authController.get("/register", (req, res) => {
   res.render("auth/register");
 });
 
-authController.post('/register', async (req, res) => {
+authController.post("/register", async (req, res) => {
   let userData = req.body;
 
   await authServices.register(userData);
 
-  res.redirect('/');
+  res.redirect("/");
 });
 
-authController.get('/login', (req, res) => {
+authController.get("/login", (req, res) => {
   res.render("auth/login");
 });
 
-authController.post('/login', async (req, res) => {
+authController.post("/login", async (req, res) => {
   const { email, password } = req.body;
-    console.log(email, password);
-    
+  console.log(email, password);
+
   try {
-      const token = await authServices.login(email, password);
-      console.log(token);
-      
+    const token = await authServices.login(email, password);
+    
+    res.cookie("auth", token);
+    res.redirect("/");
+
   } catch (error) {
     console.log(error.message);
-    return res.redirect('/404')
-    
+    return res.redirect("/404");
   }
-
-  res.redirect('/');
 });
 
 export default authController;
