1995b44a8ba70dee4b14cdee309e88e6bb14e591
Daniel Uzunski <dani.pleven@icloud.com>
2025-02-02 14:46:33 +0200

    Add login functionality
    installed jsonwebtoken

--- src/controllers/authController.js
+++ src/controllers/authController.js
@@ -1,22 +1,39 @@
-import express from 'express';
-import { authServices } from '../services/authService.js';
+import express from "express";
+import { authServices } from "../services/authService.js";
 
 const authController = express.Router();
 
-authController.get('/register', (req, res) => {    
-    res.render('auth/register');
-})
+authController.get('/register', (req, res) => {
+  res.render("auth/register");
+});
 
 authController.post('/register', async (req, res) => {
-    let userData = req.body;
+  let userData = req.body;
 
-    await authServices.register(userData);
+  await authServices.register(userData);
 
-    res.redirect('/');
+  res.redirect('/');
 });
 
 authController.get('/login', (req, res) => {
-    res.render('auth/login');
-})
+  res.render("auth/login");
+});
+
+authController.post('/login', async (req, res) => {
+  const { email, password } = req.body;
+    console.log(email, password);
+    
+  try {
+      const token = await authServices.login(email, password);
+      console.log(token);
+      
+  } catch (error) {
+    console.log(error.message);
+    return res.redirect('/404')
+    
+  }
+
+  res.redirect('/');
+});
 
 export default authController;
--- src/services/authService.js
+++ src/services/authService.js
@@ -1,13 +1,47 @@
 import User from "../models/User.js"
+import bcrypt from 'bcrypt';
+import jwt from 'jsonwebtoken';
+
+const SECRET = 'NKFnaknweonfk32423nksanokvnlewflkn'
 
 function register(userData){
-    
+
     const newUser = User.create(userData);
 
     return newUser;
 }
 
+async function login(email, password){
+    //Check if user exists
+    const user = await User.findOne({email});
+    console.log(user);
+    
+    if (!user){
+        throw new Error('Invalid email or password!');
+    }
+
+    console.log(user.password, password);
+    
+    //Check if password is correct
+    const isValid = await bcrypt.compare(password, user.password);
+    console.log(isValid);
+    
+    if (!isValid){
+        throw new Error('Invalid email or password!')
+    }
+
+    //Generate token
+    const payload = {
+        id: user.id,
+        email: user.email
+    }
+    const token = jwt.sign(payload, SECRET, {expiresIn: '2h'});
+
+    return token;
+}
+
 
 export const authServices = {
-    register
+    register,
+    login
 }
